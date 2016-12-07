0ceec504f4f7ec7d3d5db0f8c36cad4644b6e40e
Davy Peter Braun <hello@davybraun.com>
2016-12-07 16:52:49 +0100

    Implement liking and random commenting.

--- src/autogram.js
+++ src/autogram.js
@@ -9,10 +9,18 @@ const nightmare = require('nightmare')({
 });
 const cheerio = require('cheerio');
 const request = require('request-promise');
-const colors = require("colors/safe");
+const colors = require('colors/safe');
+const _e = require('node-emoji').get;
 const vo = require('vo');
 
 module.exports = (spinner, statusPrefix, hashtags, excludes, interval, maxScroll, username, password) => {
+  const randomComment = () => {
+    const messages = [
+      '<3', 'sweet <3', 'aaaaw', '<3 <3 <3', 'cool!', 'very nice', 'love it'
+    ];
+    return messages[Math.floor(Math.random() * messages.length)]
+  };
+
   const cleanedTags = hashtags => {
     return hashtags.split(' ').map(t => t);
   };
@@ -43,6 +51,7 @@ module.exports = (spinner, statusPrefix, hashtags, excludes, interval, maxScroll
   }
 
   let tags = cleanedTags(hashtags);
+  let excludedTagsList = excludes ? excludes.split(' ') : [];
 
   statusMsg(`Searching for posts tagged "#${tags.join(', #')}" every ${interval}... starting now!`);
 
@@ -52,7 +61,7 @@ module.exports = (spinner, statusPrefix, hashtags, excludes, interval, maxScroll
     let scrollIterations = 0;
 
     yield nightmare
-      .viewport(800, 600)
+      .viewport(1024, 800)
       .useragent('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36')
 
       // Go to Instagram's landing page.
@@ -82,9 +91,11 @@ module.exports = (spinner, statusPrefix, hashtags, excludes, interval, maxScroll
       });
 
     // Explore each tags.
-    while (tags.length > 0) {
+   // while (tags.length > 0) {
+      let tag = tags.shift();
+
       yield nightmare
-        .goto(`https://www.instagram.com/explore/tags/${tags.shift()}`)
+        .goto(`https://www.instagram.com/explore/tags/${tag}`)
         .wait('a[href*="?max_id"]')
 
         // Click on "load more" below content to trigger lazy-loading
@@ -110,19 +121,65 @@ module.exports = (spinner, statusPrefix, hashtags, excludes, interval, maxScroll
 
       // Content should now be ready.
       // Capture a list of hyperlinks of posts to act on.
-      let posts = yield nightmare.evaluate(() => {
+      let posts = yield nightmare.evaluate((tag) => {
         return [].map.call(
-          document.querySelectorAll('a[href*="?tagged=instagood"]'), elm => elm.href
+          document.querySelectorAll(`a[href*="?tagged=${tag}"]`), elm => elm.href
         );
-      });
+      }, tag);
+
 
       for (let i = 0; i < posts.length; i++) {
-        yield nightmare
+        let canInteract = yield nightmare
+          // Go to post page.
           .goto(posts[i])
-          .wait('article div section a[href="#"]')
-          .click('article div section a[href="#"]')
+
+          // Wait for OP's comment to be rendered,
+          // then check if I didn't leave a "like"
+          // or commented it already.
+          .wait('h1 span')
+          .evaluate((username, excludedTagsList) => {
+            const opComment = document.querySelector('h1 span').innerText;
+            const tags = opComment.split(' ').filter(o => o.indexOf('#') === 0).join(' ');
+            const userNames = [].map.call(
+              [].filter.call(
+                document.querySelectorAll('article:nth-child(1) div ul li a'),
+                elm => elm.innerText.indexOf('#') === -1
+              ),
+              elm => elm.innerText
+            );
+
+            const hasAlreadyLiked = !!document.querySelector('.coreSpriteHeartFull')
+            const hasAlreadyCommented = userNames.indexOf(username) > -1;
+            const containsExcludedHashtags = excludedTagsList.filter(t => tags.indexOf(t) > -1).length;
+
+            return hasAlreadyLiked || hasAlreadyCommented || containsExcludedHashtags;
+          }, username, excludedTagsList);
+
+        if (!canInteract) {
+          let isUnavailable = yield nightmare
+            // Check if content is unavailable.
+            .evaluate(() => {
+              return !!document.querySelector('.error-container');
+            });
+
+          if (!isUnavailable) {
+            yield nightmare
+              // Wait for page to be rendered.
+              .wait('article div section a[href="#"]')
+              .wait('form input')
+
+              // Click on like button
+              .click('article div section a[href="#"]')
+
+              // Add random comment and press ENTER to validate.
+              .type('form input', randomComment())
+              .wait(2000)
+              .type('form input', '\u000d')
+              .wait(1000);
+          }
+        }
       }
-    }
+    //}
   };
 
   vo(main)(err => {
